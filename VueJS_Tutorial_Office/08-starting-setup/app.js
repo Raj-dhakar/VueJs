24f4524024665688a6ef26590052df948dd80ac1
Raj Dhakar <raj.dhakar@oxanepartners.com>
2026-03-16 15:17:03 +0530

    Learning 8 Done

--- VueJS_Tutorial_Office/08-starting-setup/app.js
+++ VueJS_Tutorial_Office/08-starting-setup/app.js
@@ -10,9 +10,47 @@ const app = Vue.createApp({
       this.currentUserInput = event.target.value;
     },
     setText() {
-      this.message = this.currentUserInput;
+      // this.message = this.currentUserInput;
+      this.message=this.$refs.userInput.value;
     },
   },
+  beforeCreate(){ // Before App was Created
+    console.log('beforeCreate()');
+  },
+  created(){ // App is created but not mounted till Now.
+    console.log('created()');
+  },
+  beforeMount(){
+    console.log('beforeMount()');
+  },
+  mounted(){
+    console.log('mounted()');
+  },
+  beforeUpdate(){
+    console.log('Before Update!!');
+  },
+  updated(){
+    console.log('updated');
+  },
+  beforeUnmount(){
+    console.log('Before Unmount');
+  },
+  unmounted(){
+    console.log('Unmounted!!');
+  }
+});
+
+const app2=Vue.createApp({
+  template:`
+  <p> {{favouriteMeal}}</p>
+  `
+  ,
+    data(){
+      return {
+        favouriteMeal: 'Pizza'
+      }
+    }
 });
 
 app.mount('#app');
+app2.mount('#app2');
