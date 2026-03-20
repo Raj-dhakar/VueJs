aa85e13076c77cdd9a411bbc0da64ec1a7af1344
Raj Dhakar <raj.dhakar@oxanepartners.com>
2026-03-20 15:32:10 +0530

    Learning 16 Partail Done

---
+++ VueJS_Tutorial_Office/16-routing-01-starting-setup/src/main.js
@@ -0,0 +1,28 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path:'/', redirect:'/teams'},
+        { name:'teams', path: '/users', component: UsersList, alias: '/' },
+        {
+            path: '/teams', component: TeamsList, 
+            children: [
+                { name:'team-member', path: ':teamId', component: TeamMembers, props: true },
+            ]
+        },
+        { path: '/:notFound(.*)', redirect: '/teams' },
+    ]
+});
+
+const app = createApp(App)
+
+app.use(router);
+
+app.mount('#app');
